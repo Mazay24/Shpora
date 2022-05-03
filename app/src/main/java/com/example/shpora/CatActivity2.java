9ff6604aade319c056b79f4c99d2b606a7125159
olchh <ugarrova@mail.ru>
2022-05-03 20:53:45 +0800

    qq

--- app/src/main/java/com/example/shpora/CatActivity2.java
+++ app/src/main/java/com/example/shpora/CatActivity2.java
@@ -2,7 +2,9 @@ package com.example.shpora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class CatActivity2 extends AppCompatActivity {
 
@@ -10,6 +12,9 @@ public class CatActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat2);
+        Intent intent = getIntent();
+        WebView textView = findViewById(R.id.catiweb);
+        textView.loadUrl("file:///android_asset/" + intent.getStringExtra("key"));
 
     }
 }
--- app/src/main/java/com/example/shpora/MainActivity.java
+++ app/src/main/java/com/example/shpora/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.shpora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +19,17 @@ public class MainActivity extends AppCompatActivity {
 
         ListView CatList = findViewById(R.id.cat);
         String[] catty = getResources().getStringArray(R.array.delaet);
-        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, catty);
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, R.layout.activity_stile, catty);
         CatList.setAdapter(adapter);
+        Intent intent = new Intent(this, CatActivity2.class);
+        CatList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+            {
+                intent.putExtra("key", ((TextView)v).getText() + ".html");
+                startActivity(intent);
+            }
+        });
     }
 }
