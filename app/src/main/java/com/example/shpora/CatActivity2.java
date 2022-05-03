039c045fee1e0c86b3fa96cdba95b9aaf6cef541
Nikita <nik.isaev2004@mail.ru>
2022-05-03 18:29:09 +0300

    Дизайн сделад

--- app/src/main/java/com/example/shpora/CatActivity2.java
+++ app/src/main/java/com/example/shpora/CatActivity2.java
@@ -10,5 +10,6 @@ public class CatActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat2);
+            
     }
 }
